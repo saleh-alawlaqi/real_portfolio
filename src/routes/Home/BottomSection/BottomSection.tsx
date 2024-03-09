ad89f756e3bd66205872ac9fd51294064511361d
Alex <alexsouske18@gmail.com>
2024-03-09 00:37:46 +0000

    Update bottom section padding in Home page

--- src/routes/Home/BottomSection/BottomSection.tsx
+++ src/routes/Home/BottomSection/BottomSection.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const BottomSection = () => {
     return (
-        <div className="flex flex-col z-10 items-center bg-slate-50 pb-20 w-full">
+        <div className="flex flex-col z-10 items-center bg-slate-50 pb-12 w-full">
             <div className="flex flex-col w-full lg:max-w-[1300px]">
                 <Features />
                 <CTA />
