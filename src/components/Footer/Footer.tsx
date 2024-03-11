1a754f6d97ad4c7f52a46b2246c243d89ab76cb3
Alex <alexsouske18@gmail.com>
2024-03-11 19:12:08 +0000

    Refactor Footer component styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,10 +6,7 @@ const Footer = () => {
             {/* <div className="footer-blur-1 absolute top-0 left-0 w-1/2 h-[15rem]"></div> */}
             {/* <div className="footer-blur-2 absolute top-0 right-0 w-1/2 h-[15rem]"></div> */}
             <div className="footer-container footer-bg p-8 gap-8 flex flex-col xl:flex-row items-center justify-between w-full md:p-8 lg:p-10">
-                <NavLink
-                    to={"/"}
-                    className="font-inter_black text-gradient text-[20px] lg:text-[22px]"
-                >
+                <NavLink to={"/"} className="font-inter_black  text-[20px] lg:text-[22px]">
                     AlexDevStudio.
                 </NavLink>
                 <ul className="flex flex-wrap justify-center gap-5 lg:gap-2 items-center text-gray-900 dark:text-white">
